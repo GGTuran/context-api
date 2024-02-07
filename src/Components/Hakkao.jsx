2dbf8d2bad4b2d24b9cb9098ff0a306ef135de2b
GGTuran <tayyabturan67@gmail.com>
2024-02-07 13:45:27 +0600

    rafce

--- src/Components/Hakkao.jsx
+++ src/Components/Hakkao.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-const Hakkao = props => {
+const Hakkao = () => {
   return (
     <div>Hakkao</div>
   )
 }
-
-Hakkao.propTypes = {}
 
 export default Hakkao
